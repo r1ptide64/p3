d5e60d938ec1515de191a1815b93914a14d67568
jgordon <josh.h.gordon@gmail.com>
2016-03-12 01:05:45 -0600

    got most requirements up and running

--- mem.c
+++ mem.c
@@ -17,30 +17,40 @@ void *Mem_Init(int sizeOfRegion) {
 	int fd = open("/dev/zero", O_RDWR);
 	int pageSize = getpagesize();
 	sizeOfRegion = (1 + (sizeOfRegion/pageSize)) * pageSize;
-	void *regionPtr = mmap(NULL, sizeOfRegion, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
+	void *regionPtr = mmap(NULL, sizeOfRegion, PROT_READ | PROT_WRITE,
+						   MAP_PRIVATE, fd, 0);
 	close(fd);
 	if (regionPtr == MAP_FAILED)
 		return NULL;
 	head = regionPtr;
 	head->size = sizeOfRegion;
+	head->next = NULL;
 	return regionPtr;
 }
-void *GetDataPtr(Block *block) {
-	return block + sizeof(Block);
+
+void *GetDataPointer(Block *block) {
+	return block + 1;
 }
 
-void *GetNextBlockAddr(Block *block) {
+void *GetNextAvailableAddress(Block *block) {
 	if (block == head)
-		return GetDataPtr(block);
-	return GetDataPtr(block) + block->size;
+		return GetDataPointer(block);
+	void *tmp = GetDataPointer(block);
+	return tmp + block->size;
 }
 
 int WillNewBlockFit(Block *currBlock, int size) {
-	void *lastDataAddr = GetDataPtr(GetNextBlockAddr(currBlock));
+	Block *tmpBlock = GetNextAvailableAddress(currBlock);
+//	printf("tmpBlock: %p\n", tmpBlock);
+	void *lastNeededDataAddress = GetDataPointer(tmpBlock) + size;
+//	printf("lastNeededDataAddress: %p\n", lastNeededDataAddress);
+	void *lastAvailableDataAdress;
 	if (currBlock->next == NULL)
-		return lastDataAddr < head + head->size;
+		lastAvailableDataAdress = (void *)head + head->size;
 	else
-		return lastDataAddr < currBlock->next;
+		lastAvailableDataAdress = (void *)(currBlock->next);
+//	printf("lastAvailableDataAddress: %p\n", lastAvailableDataAdress);
+	return lastNeededDataAddress <= lastAvailableDataAdress;
 }
 
 void *Mem_Alloc(int size) {
@@ -55,31 +65,71 @@ void *Mem_Alloc(int size) {
 	if (!WillNewBlockFit(currBlock, size)) {
 		return NULL;
 	}
-	Block *newBlock = GetLastAddressUsedByBlock(currBlock) + 1;
+	newBlock = GetNextAvailableAddress(currBlock);
 	newBlock->next = currBlock->next;
 	currBlock->next = newBlock;
 	newBlock->size = size;
-	return GetDataPtr(newBlock);
+	return GetDataPointer(newBlock);
+}
+
+int Mem_Free(void *ptr) {
+//	printf("in mem free!\n");
+	if (ptr == NULL || head == NULL)
+		return -1;
+//	printf("line 75!\n");
+	void *headPointer = (void *)head;
+	void *lastDataPointer = headPointer + head->size;
+	if (ptr <= headPointer || ptr > lastDataPointer)
+		return -1;
+//	printf("line 79!\n");
+	Block *prevBlock = head;
+	Block *currBlock = head->next;
+//	printf("about to start loop!");
+	while ( currBlock != NULL) {
+//		printf("currBlock=%p\n", currBlock);
+		if (GetDataPointer(currBlock) == ptr) {
+			prevBlock->next = currBlock->next;
+			return 0;
+		}
+		prevBlock = currBlock;
+		currBlock = currBlock->next;
+	}
+	return -1;
+}
+
+void Mem_Dump() {
+	Block *prevBlock = head;
+	Block *currBlock = head->next;
+	void *currAddr;
+	void *lastAddressUsedByPrevBlock;
+	while (currBlock != NULL) {
+		currAddr = currBlock;
+		lastAddressUsedByPrevBlock = GetNextAvailableAddress(prevBlock);
+		if (lastAddressUsedByPrevBlock < currAddr)
+			printf("%p - %p\n", lastAddressUsedByPrevBlock, currAddr - 1);
+		prevBlock = currBlock;
+		currBlock = currBlock->next;
+	}
+	lastAddressUsedByPrevBlock = GetNextAvailableAddress(prevBlock);
+	currAddr = (void *)head + head->size;
+	if (lastAddressUsedByPrevBlock < currAddr)
+		printf("%p - %p\n", lastAddressUsedByPrevBlock, currAddr - 1);
 }
 
 int main() {
-	Mem_Init(20000);
+	Mem_Init(8000);
+	printf("===initial dump===\n");
+	Mem_Dump();
+	void *tmp[16];
 	int i;
-	printf("head: %p\n", head);
-	for (i=1; i < 10; i++) {
-		printf("block %d: %p\n", i, Mem_Alloc(100));
+	for (i = 0; i < 16; i++) {
+		tmp[i] = Mem_Alloc(32);
 	}
-	Block *currBlock = head;
-	i = 0;
-	while (currBlock!=NULL) {
-		printf("===block %d===\n", i);
-		printf("blockAddr: %p\n", currBlock);
-		printf("block->size: %d\n", currBlock->size);
-		printf("block->next: %p\n", currBlock->next);
-		printf("dataptr: %p\n", GetDataPtr(currBlock));
-		printf("\n");
-		i++;
-		currBlock = currBlock->next;
+	printf("===post-malloc dump===\n");
+	Mem_Dump();
+	for (i = 0; i < 16; i += 3) {
+		Mem_Free(tmp[i]);
 	}
-	exit(0);
+	printf("===post-free dump===\n");
+	Mem_Dump();
 }
